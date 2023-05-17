3c39ee88f9aaf345ac6889e4c85db0d485949785
Francesco Negri <francesco.negri@outlook.com>
2023-05-17 12:31:20 -0500

    Fix overwritten params

--- getBaselineFromStim.m
+++ getBaselineFromStim.m
@@ -1,8 +1,6 @@
 function [baseline, baselinePercentiles] = getBaselineFromStim(data, stim, sampleRate, baselineDuration, baselineOffset, percentiles)
 %GETBASELINEFROMSTIM Summary of this function goes here
 %   Detailed explanation goes here
-baselineDuration = 10e-3;
-baselineOffset = 0.5e-3;
 
 baselineSamples = round(baselineDuration * sampleRate):-1:1;
 baselineSamples = baselineSamples + round(baselineOffset * sampleRate);
